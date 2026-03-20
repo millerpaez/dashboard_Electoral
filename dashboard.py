bcb9b0be1721423a21ee9f5f626841eab5ae77ae
miller.paez <miller.paez1@gmail.com>
2026-03-19 21:36:11 -0500

    Quitar cache para datos siempre frescos

--- dashboard.py
+++ dashboard.py
@@ -6,7 +6,6 @@ import re
 import gspread
 from google.oauth2.service_account import Credentials
 
-@st.cache_data
 def load_data():
     creds = Credentials.from_service_account_info(
         st.secrets["gcp_service_account"],
